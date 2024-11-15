59a3f95ba894fab24c304094bf44999bd755fb33
KreativeThinker <sagely2005eagle@gmail.com>
2024-11-15 20:20:11 +0530

    chore: revert to server

--- LifeFence/constants/apiConfig.ts
+++ LifeFence/constants/apiConfig.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://192.168.108.83:5000/api";
+// export const BASE_URL = "http://192.168.95.143:5000/api";
+export const BASE_URL = "https://marvelous-hope-production.up.railway.app/api";
