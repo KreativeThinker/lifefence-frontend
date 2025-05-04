f31a3e56329756c041f6833d4f602030c610543c
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-04 21:15:27 +0530

    chore: update url

--- LifeFence/constants/apiConfig.ts
+++ LifeFence/constants/apiConfig.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://192.168.95.143:5000/api";
-export const BASE_URL = "https://marvelous-hope-production.up.railway.app/api";
+export const BASE_URL = "http://192.168.236.143:8080/api";
+// export const BASE_URL = "https://marvelous-hope-production.up.railway.app/api";
